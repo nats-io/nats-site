af6f10879eee8b3d2f4d12bbc32bce99433c2887
lucperkins <lucperkins@gmail.com>
2020-01-22 09:10:36 -0800

    Move JS to assets folder and remove unused Gulp config
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,7 @@
 var gulp         = require('gulp'),
-    concat       = require('gulp-concat'),
-    // watch        = require('gulp-watch'),
-    less         = require('gulp-less'),
-    uglify       = require('gulp-uglify'),
-    minifyCSS    = require('gulp-clean-css'),
     gm           = require('gulp-gm'),
     imageResize  = require('gulp-image-resize'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps   = require('gulp-sourcemaps'),
-    clean        = require('gulp-clean'),
-    shell        = require('gulp-shell'),
-    runSequence  = require('run-sequence');
-
-// LESS
-gulp.task('less', function() {
-  return gulp.src('src/less/styles.less')
-    .pipe(less())
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer())
-    .pipe(minifyCSS())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('static/css'));
-});
-
-gulp.task('css', function() {
-  return gulp.src('src/css/**/*.css')
-      .pipe(gulp.dest('static/css'));
-});
-
-// Javascript
-gulp.task('js', function() {
-  return gulp.src(['src/js/anchor.js', 'src/js/moment.js', 'src/js/**/*.js'])
-    .pipe(concat('index.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('static/js'));
-});
-
-// Images
-gulp.task('img', function() {
-  return gulp.src('src/img/**/*')
-    .pipe(gulp.dest('static/img'));
-});
-
-// Fonts
-gulp.task('font', function() {
-  return gulp.src('src/fonts/**/*')
-    .pipe(gulp.dest('static/fonts'));
-});
+    clean        = require('gulp-clean');
 
 // Resize User Logos
 gulp.task('userLogos', function() {
@@ -66,12 +21,6 @@ gulp.task('partnerLogos', function() {
     .pipe(gulp.dest('static/img/partner_logos'));
 });
 
-// Resize Documentaiton Images
-gulp.task('docsImages', function() {
-  return gulp.src('src/documentation/**/*.{png,jpg,jpeg}')
-    .pipe(gulp.dest('static/img/documentation'));
-});
-
 // Resize Blog Images
 gulp.task('blogImages', function() {
   return gulp.src('src/blog/**/*.{png,jpg,jpeg}')
@@ -81,50 +30,8 @@ gulp.task('blogImages', function() {
     .pipe(gulp.dest('static/img/blog'));
 });
 
-// Will have to improve this later on
-gulp.task('blogImagesGifs', function() {
-  return gulp.src('src/blog/**/*.gif')
-    .pipe(gulp.dest('static/img/blog'));
-});
-
-// Will have to improve this later on
-gulp.task('collateral', function() {
-  return gulp.src('src/collateral/*')
-    .pipe(gulp.dest('static/collateral'));
-});
-
-// Clean
-gulp.task('clean', function() {
-  return gulp.src(['public', 'static'], {read: false})
-    .pipe(clean());
-});
-
-// HUGO
-gulp.task('hugo', shell.task('hugo --minify'));
-
-// // Watch
- gulp.task('watch', function() {
-   gulp.watch('src/less/**/*.less', [ 'less' ]);
-   gulp.watch('src/js/**/*.js', [ 'js' ]);
-   gulp.watch('src/img/**/*', [ 'img' ]);
-   gulp.watch('src/font/**/*', [ 'font' ]);
-   gulp.watch('src/user_logos/**/*', [ 'userLogos' ]);
-   gulp.watch('src/partner_logos/**/*', [ 'partnerLogos' ]);
-   gulp.watch('src/company_logos/**/*', [ 'companyLogos' ]);
-   gulp.watch('src/blog/**/*', [ 'blogImages' ]);
-   gulp.watch('src/blog/**/*', [ 'blogImagesGifs' ]);
-});
-
 gulp.task('help', function(){
   console.log('if gulp build fails, simply add ".pipe(plumber())" to the task to see the error');
 });
 
-gulp.task('assets', [ 'less', 'css', 'js', 'img', 'font', 'docsImages', 'userLogos', 'partnerLogos', 'blogImages', 'blogImagesGifs', 'collateral']);
-
-// Default Task
-// gulp.task('default', [ 'less', 'js', 'img', 'font', 'docsImages', 'userLogos', 'partnerLogos', 'blogImages', 'blogImagesGifs', 'watch' ]);
-
-// Build for Production
-gulp.task('build', function (callback) {
-  runSequence( 'clean', 'assets', 'hugo', callback);
-});
+gulp.task('assets', ['userLogos', 'partnerLogos', 'blogImages']);
